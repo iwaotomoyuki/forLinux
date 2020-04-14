440408763f5ad19bfc1bfa721a55a78836b549e9
iwao <iwao@dpc-net.co.jp>
2020-04-13 17:35:25 -0700

    GItk用のコミット

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
+#include <stdio.h>
+
+static int total = 0;
+
+void init(void);
+void add(int x);
+int get(void);
+
+void init(void)
+{
+	total = 0;
+}
+
+void add(int x)
+{
+	total += x;
+}
+
+int get(void)
+{
+	return total;
+}
+
 int main(void)
 {
-	int a = 0;
+	int i = 0;
+
+	init();
+	for(i = 0; i < 1000; i++) {
+		add(i);
+	}
+	printf("total = %d\n", get());
 	return 0;
 }
